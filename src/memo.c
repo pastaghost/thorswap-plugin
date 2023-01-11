adf4059e66677e4830995f3f0d841168851e282d
pastaghost <pastaghost@2520.io>
2023-01-10 21:17:58 -0700

    refactor: memo parser

---
+++ src/memo.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <string.h>
+#include "memo.h"
+
+memo_t parsed_memo = {0};
+
+int parse_memo(memo_t *memo, char *str) {
+    char delim[] = ":";  // Field delimiter
+    char *token = NULL;
+    char cpy_str[250] = {'\0'};
+    char *cpy_str_addr = (char *) &cpy_str;  // strsep() requires char** as the first argument
+
+    if (str == NULL || memo == NULL || str[0] == '\0') {  // Sanity checks
+        return ERROR_INVALID_PARAMETERS;
+    }
+
+    memset(memo, 0, sizeof(memo_t));  // Clear out any junk that might be in struct
+    strcpy(cpy_str, str);  // Copy str into new buffer because strsep() modifies the input buffer
+    uint8_t *fields[] = {
+        NULL,  // Leave me set to NULL since we're throwing away the message prefix (s,=,etc.)
+        (uint8_t *) &(memo->asset),
+        (uint8_t *) &(memo->dest_addr),
+        (uint8_t *) &(memo->limit),
+        (uint8_t *) &(memo->affiliate),
+        (uint8_t *) &(memo->fee),
+        (uint8_t *) &(memo->dex_aggregator_addr),
+        (uint8_t *) &(memo->final_token_addr),
+        (uint8_t *) &(memo->min_amount_out)};
+
+    token = strsep(&cpy_str_addr, delim);
+
+    // TODO: Is "swap" a valid message prefix?
+    if (strcmp(token, "=") && strcmp(token, "s") &&
+        strcmp(token, "SWAP")) {  // Check for invalid message prefix
+        return ERROR_INVALID_MEMO_PREFIX;
+    }
+
+    /* Parse string data into struct */
+    for (uint8_t i = 1; i < (sizeof(fields) / sizeof(uint8_t *)); i++) {
+        token = strsep(&cpy_str_addr, delim);
+        if (*token != '\0') {
+            strcpy((char *) fields[i], token);
+            memo->fields |= 1UL << (i - 1);
+        }
+    }
+
+    return validate_memo(memo);
+}
+
+int validate_memo(memo_t *memo) {
+    /* memo->asset, memo->dest_addr are always required */
+    if (!HAS_ASSET(memo)) return ERROR_MISSING_ASSET;
+    if (!HAS_DEST_ADDR(memo)) return ERROR_MISSING_DEST_ADDR;
+
+    /* memo->affiliate is required if fee is set*/
+    if (!HAS_AFFILIATE(memo) && HAS_FEE(memo)) return ERROR_MISSING_AFFILIATE;
+
+    /* memo->final_token_addr, memo->min_amount_out required if memo->dex_aggregator_addr is set*/
+    if (HAS_DEX_AGGREGATOR_ADDR(memo)) {
+        if (!HAS_FINAL_TOKEN_ADDR(memo)) {
+            return ERROR_MISSING_FINAL_TOKEN_ADDR;
+        }
+        if (!HAS_MIN_AMOUNT_OUT(memo)) {
+            return ERROR_MISSING_MIN_AMOUNT_OUT;
+        }
+    }
+    /* memo->dex_aggregator_addr required if memo->final_token_addr, memo->min_amount_out are set */
+    if (!HAS_DEX_AGGREGATOR_ADDR(memo) &&
+        (HAS_FINAL_TOKEN_ADDR(memo) || HAS_MIN_AMOUNT_OUT(memo))) {
+        return ERROR_MISSING_DEX_AGGREGATOR_ADDR;
+    }
+    return 0;
+}
--- src/memo.h
+++ src/memo.h
@@ -2,14 +2,47 @@
 #define _MEMO_H_
 
 #include <stdint.h>
-#include "constants.h"
+
+#define HAS_ASSET(x)               ((x)->fields & 1UL)
+#define HAS_DEST_ADDR(x)           ((x)->fields & 1UL << 1)
+#define HAS_LIMIT(x)               ((x)->fields & 1UL << 2)
+#define HAS_AFFILIATE(x)           ((x)->fields & 1UL << 3)
+#define HAS_FEE(x)                 ((x)->fields & 1UL << 4)
+#define HAS_DEX_AGGREGATOR_ADDR(x) ((x)->fields & 1UL << 5)
+#define HAS_FINAL_TOKEN_ADDR(x)    ((x)->fields & 1UL << 6)
+#define HAS_MIN_AMOUNT_OUT(x)      ((x)->fields & 1UL << 7)
+
+/** __builtin_popcount() is a compiler intrinsic available in both GCC and Clang.
+ * Note that this may not work on other compilers. */
+#define NUM_MEMO_FIELDS(x) (__builtin_popcount((x)->fields))
+
+enum errors {
+    ERROR_NONE = 0,
+    ERROR_INVALID_PARAMETERS,
+    ERROR_INVALID_MEMO_PREFIX,
+    ERROR_MISSING_ASSET,
+    ERROR_MISSING_DEST_ADDR,
+    ERROR_MISSING_AFFILIATE,
+    ERROR_MISSING_DEX_AGGREGATOR_ADDR,
+    ERROR_MISSING_FINAL_TOKEN_ADDR,
+    ERROR_MISSING_MIN_AMOUNT_OUT
+};
 
 typedef struct memo_t {
-    uint8_t asset[61];
-    uint8_t dest_addr[41];
-    uint8_t limit[10];      // Trade limit in 1e8 precision
-    uint8_t affiliate[44];  // THORChain address length = 43, max THORName length = 30
-    uint8_t fee[5];         // Fee, from 0-1000 basis points
+    char asset[61];
+    char dest_addr[41];
+    char limit[10];      // Trade limit in 1e8 precision
+    char affiliate[44];  // THORChain address length = 43, max THORName length = 30
+    char fee[5];         // Fee, from 0-1000 basis points
+    char dex_aggregator_addr[41];
+    char final_token_addr[41];
+    char min_amount_out[30];  // TODO: What is the maximum number of characters here?
+    uint8_t fields;
 } memo_t;
+
+int parse_memo(memo_t *memo, char *str);
+int validate_memo(memo_t *memo);
+
+extern memo_t parsed_memo;
 
 #endif
