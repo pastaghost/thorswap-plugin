4f38de466898bc6ac0b4e8de061d8f3bd0a5dfb1
pastaghost <pastaghost@2520.io>
2023-01-10 21:20:06 -0700

    refactor: screen count calculation

--- src/handle_finalize.c
+++ src/handle_finalize.c
@@ -1,4 +1,5 @@
 #include "thorchain_router_plugin.h"
+#include "memo.h"
 
 void handle_finalize(void *parameters) {
     ethPluginFinalize_t *msg = (ethPluginFinalize_t *) parameters;
@@ -6,8 +7,7 @@ void handle_finalize(void *parameters) {
 
     msg->uiType = ETH_UI_TYPE_GENERIC;
 
-    /* [VAULT, ASSET, AMOUNT, ...Variable number of memo fields] */
-    msg->numScreens = 3 + context->memo_num_fields;
+    msg->numScreens = 3 + NUM_MEMO_FIELDS(context->memo);
 
     // EDIT THIS: set `tokenLookup1` (and maybe `tokenLookup2`) to point to
     // token addresses you will info for (such as decimals, ticker...).
